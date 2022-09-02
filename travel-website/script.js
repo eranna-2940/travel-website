0b0a2cf86ba0544ac8028ae36203692d1babad92
GUTTI ERANNA <81354309+eranna-2940@users.noreply.github.com>
2022-09-02 16:43:01 +0530

    Add files via upload

---
+++ travel-website/script.js
@@ -0,0 +1,128 @@
+let navbar = document.querySelector('.header .navbar');
+
+document.querySelector('#menu-button').onclick = () =>{
+    navbar.classList.add('active');
+}
+
+document.querySelector('#nav-close').onclick = () =>{
+    navbar.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-button').onclick = () =>{
+    searchForm.classList.add('active');
+}
+
+document.querySelector('#close-search').onclick = () =>{
+    searchForm.classList.remove('active');
+}
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+
+    if(window.scrollY > 0){
+        document.querySelector('.heading').classList.add('active');
+    }else{
+        document.querySelector('.heading').classList.remove('active');
+    }
+};
+window.onload = () =>{
+    if(window.scrollY > 0){
+        document.querySelector('.heading').classList.add('active');
+    }else{
+        document.querySelector('.heading').classList.remove('active');
+    }
+};
+const swiper = new Swiper(".home-slider", {
+    loop:true, 
+    grabCursor:true,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+const swipers = new Swiper(".product-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
+const swiperr = new Swiper(".review-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+const swip = new Swiper(".blogs-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 10,
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        991: {
+          slidesPerView: 3,
+        },
+    },
+});
+
+const swipe = new Swiper(".clients-slider", {
+    loop:true, 
+    grabCursor:true,
+    spaceBetween: 20,
+    breakpoints: {
+        0: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 3,
+        },
+        1024: {
+          slidesPerView: 4,
+        },
+    },
+});
