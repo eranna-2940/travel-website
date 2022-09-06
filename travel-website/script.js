01524ff93c09345fda53eb4f387033f204160fdc
GUTTI ERANNA <81354309+eranna-2940@users.noreply.github.com>
2022-09-06 11:45:45 +0530

    Update script.js

--- travel-website/script.js
+++ travel-website/script.js
@@ -1,3 +1,76 @@
+let formBtn = document.querySelector('#login-btn');
+let loginForm = document.querySelector('.login-form-container');
+let formClose = document.querySelector('#form-close');
+
+
+
+window.onscroll = () =>{
+  loginForm.classList.remove('active');
+}
+formBtn.addEventListener('click', () =>{
+  loginForm.classList.add('active');
+});
+
+formClose.addEventListener('click', () =>{
+  loginForm.classList.remove('active');
+});
+
+
+function validemail() {
+  let email = document.getElementById("email").value
+  let erroremail = document.getElementById("erroremail")
+
+  if (email.endsWith('gmail')) {
+      erroremail.innerHTML = ""
+  } else {
+      erroremail.innerHTML = "please enter gmail.com";
+      erroremail.style.color = 'red';
+  }
+}
+
+function validpassword() {
+  let password = document.getElementById("password").value;
+  let errorpassword = document.getElementById("errorpassword");
+
+  if (password.length < 3) {
+      errorpassword.innerHTML = "please enter strong password"
+      errorpassword.style.color = 'red';
+  } else {
+
+      errorpassword.innerHTML = ""
+  }
+}
+
+
+function getFormvalue() {
+
+  //sessionStorage.setItem('email',r_email.value)
+  //sessionStorage.setItem('password',r_password.value)
+
+
+  let r_email = document.getElementById("email");
+  let r_password = document.getElementById("password");
+
+  localStorage.setItem('email', r_email.value);
+  localStorage.setItem('password', r_password.value);
+
+
+
+  if(email.value == "" || password.value == ""){
+      alert('please filled all inputes')
+  }
+  document.write(`
+          {Email:${r_email.value}<br>
+          Password:${r_password.value}}
+
+      `);
+
+};
+
+
+
+
+
 let navbar = document.querySelector('.header .navbar');
 
 document.querySelector('#menu-button').onclick = () =>{
